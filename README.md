17bf0c96c35d6397648fe88e5a88d2825cac7387
laurasjparis <lsjimenezp@eafit.edu.co>
2025-02-07 19:07:48 -0500

    cambios de nombre

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # smart_wallet
+
+
+python3 -m ~/dev_study/smart_wallet/.venv 
+
+source ~/dev_study/smart_wallet/.venv/bin/activate
+
+python3 -m pip install django
+
--- smartwallet/urls.py
+++ smartwallet/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from revenue import views as reviews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',reviews.home)
 ]
