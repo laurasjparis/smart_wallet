379acf5089797334e0773ff58403c744689391b7
mariafer26 <157810200+mariafer26@users.noreply.github.com>
2025-02-09 18:59:57 -0500

    Add files via upload

---
+++ SmartWallet-mafe/smartwallet/smartW/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SmartwConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "smartW"
---
+++ SmartWallet-mafe/smartwallet/smartW/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.db.models import Sum
+from .models import Saving
+
+# Create your views here.
+
+def dashboard 
